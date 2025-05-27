f74737ad58961600a7fae21669aa50e3789bcb76
Zebado <sbasualdo.antares@gmail.com>
2025-05-27 12:07:21 -0300

    update open door

--- Assets/Scripts/Others/DoorAnimation.cs
+++ Assets/Scripts/Others/DoorAnimation.cs
@@ -1,32 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class DoorAnimation : MonoBehaviour
 {
     [SerializeField] Animator _anim;
 
+    public static event Action<DoorAnimation> OnAnyPlayerEnter;
+    public static event Action<DoorAnimation> OnAnyPlayerExit;
+
+    private bool doorOpen = false;
+
     private void OpenDoor()
     {
         _anim.SetBool("Abrir", true);
+        doorOpen = true;
     }
+
     private void CloseDoor()
     {
-        _anim.SetBool("cerrado", true);
+        _anim.SetBool("Abrir", false);
+        doorOpen = false;
+    }
+
+    public void TryOpenDoor()
+    {
+        if (!doorOpen)
+        {
+            OpenDoor();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            OpenDoor();
+            OnAnyPlayerEnter?.Invoke(this);
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            CloseDoor();
+            OnAnyPlayerExit?.Invoke(this);
+            CloseDoor(); // opcional
         }
     }
 }
---
+++ Assets/Scripts/Ui_Interactive.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ui_Interactive : MonoBehaviour
+{
+    [SerializeField] GameObject pressEText;
+
+    private DoorAnimation currentDoor;
+
+    private void OnEnable()
+    {
+        DoorAnimation.OnAnyPlayerEnter += HandlePlayerEnterDoor;
+        DoorAnimation.OnAnyPlayerExit += HandlePlayerExitDoor;
+    }
+
+    private void OnDisable()
+    {
+        DoorAnimation.OnAnyPlayerEnter -= HandlePlayerEnterDoor;
+        DoorAnimation.OnAnyPlayerExit -= HandlePlayerExitDoor;
+    }
+
+    private void Update()
+    {
+        if (currentDoor != null && Input.GetKeyDown(KeyCode.I))
+        {
+            currentDoor.TryOpenDoor();
+            pressEText.SetActive(false);
+            currentDoor = null;
+        }
+    }
+
+    private void HandlePlayerEnterDoor(DoorAnimation door)
+    {
+        currentDoor = door;
+        pressEText.SetActive(true);
+    }
+
+    private void HandlePlayerExitDoor(DoorAnimation door)
+    {
+        if (currentDoor == door)
+        {
+            currentDoor = null;
+            pressEText.SetActive(false);
+        }
+    }
+}
