66bc1a11df1f692a34e0a087d91074c636dfc574
patriciomalvasiomaddalena <130863754+patriciomalvasiomaddalena@users.noreply.github.com>
2025-08-08 18:00:03 -0300

    DASH AWAY THE ANGER!
    here I dash beneath the warm fucked up code base...
    
    the csharp causing a fucking lot of null reference...

--- Assets/ScriptableObjects/Abilities/Player Abilities/Basic/Dash/Dash_Skill.cs
+++ Assets/ScriptableObjects/Abilities/Player Abilities/Basic/Dash/Dash_Skill.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
-public class Dash_Skill : MonoBehaviour
+public class Dash_Skill : Visceral_SkillLogic
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] Player_Movement _KCCMotorMovement;
+    [SerializeField] Player_Base _Base;
+    [SerializeField] private float _DashStrenght;
+
+    public override void ActivateSkill()
     {
-        
+        var Input = _Base._Player_InputActions.Gameplay.Movement.ReadValue<Vector2>();
+        var MovementDir = new
+        {
+            DirX = Input.x,
+            DirY = Input.y,
+        };
+        Transform UpDirector = _KCCMotorMovement.KKCMotor.Transform;
+
+        Vector3 DashMovement = new Vector3(MovementDir.DirX, 0, MovementDir.DirY).normalized;
+        Vector3 FinalDashMovement = Quaternion.Euler(0, UpDirector.eulerAngles.y, 0) * DashMovement;
+        FinalDashMovement *= _DashStrenght;
+        _KCCMotorMovement.AddExternalVelocity(FinalDashMovement, true);
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Initialize(Visceral_AbilitySO data, Visceral_SkillManager Skmanager, PlayerContext UserContext = null)
     {
-        
+        base.Initialize(data, Skmanager, UserContext);
+
+        _KCCMotorMovement = UserContext.GetComponent<Player_Movement>();
+        if(_KCCMotorMovement == null)
+        {
+            _KCCMotorMovement = UserContext.GetComponentInChildren<Player_Movement>();
+        }
+        _Base = UserContext.GetComponent<Player_Base>();
+
+        if(_Base == null)
+        {
+            _Base = UserContext.GetComponentInChildren<Player_Base>(true);
+        }
+
     }
 }
