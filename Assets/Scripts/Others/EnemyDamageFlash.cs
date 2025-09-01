5470149a96ea36b28d1b320522fc32b0a2f0695d
Lucas-1812 <lucasetorres2013@gmail.com>
2025-09-01 19:18:14 -0300

    Flash blanco en los enemigos cuando reciben dmg

---
+++ Assets/Scripts/Others/EnemyDamageFlash.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HostileNPC_HealthComp))]
+public class EnemyDamageFlash : MonoBehaviour
+{
+    public Renderer rend;
+    public float flashDuration = 0.2f;
+    private Material mat;
+    private float flashTimer;
+
+    private HostileNPC_HealthComp healthComp;
+
+    void Start()
+    {
+        if (rend == null)
+            rend = GetComponentInChildren<Renderer>(); 
+
+        mat = rend.material;
+
+        
+        healthComp = GetComponent<HostileNPC_HealthComp>();
+
+        
+        if (healthComp != null)
+            healthComp.OnDamaged += TriggerFlash;
+    }
+
+    void TriggerFlash()
+    {
+        flashTimer = flashDuration; 
+    }
+
+    void Update()
+    {
+        if (flashTimer > 0)
+        {
+            flashTimer -= Time.deltaTime;
+            float t = flashTimer / flashDuration;
+            mat.SetFloat("_HitFlash", 1 - t); 
+        }
+        else
+        {
+            mat.SetFloat("_HitFlash", 0);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (healthComp != null)
+            healthComp.OnDamaged -= TriggerFlash;
+    }
+}
