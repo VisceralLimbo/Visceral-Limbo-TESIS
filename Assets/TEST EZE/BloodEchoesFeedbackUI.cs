c3bff84c7e031511e072d7ca7e5d236a5393d689
Ezecxz <brian.roman@davinci.edu.ar>
2025-09-25 23:02:03 -0300

    Feedback de obtencion y reduccion bloodechoes in game
    falta anim pero subo para no tener problemas con testing je

---
+++ Assets/TEST EZE/BloodEchoesFeedbackUI.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class BloodEchoesFeedbackUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI feedbackText;
+
+    // tiempo para q se muestre y desaparezca
+    private const float DisplayDuration = 2f;
+    private Coroutine displayCoroutine;
+    public Image bloodechoes;
+
+    private void Awake()
+    {
+
+        //q empiece apagado
+        if (feedbackText != null)
+        {
+            feedbackText.gameObject.SetActive(false);
+            bloodechoes.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        // se suscribe al evento cuando se actualizan los echoes
+        BloodEchoesManager.OnBloodEchoesChanged += ShowFeedback;
+    }
+
+    private void OnDisable()
+    {
+        // se desuscribe para evitar bugs
+        BloodEchoesManager.OnBloodEchoesChanged -= ShowFeedback;
+    }
+
+    // se llama cada q se cambian los bloodechoes
+    private void ShowFeedback(float currentBloodEchoes, float deltaAmount)
+    {
+        // se muestra si hubo cambio
+        if (deltaAmount == 0 || feedbackText == null) return;
+
+        // asigno el color y el signo +|-
+        if (deltaAmount > 0)
+        {
+            // si es ganancia verde y +
+            feedbackText.color = Color.green;
+            feedbackText.text = $"+{deltaAmount.ToString("F0")} Blood Echoes";
+        }
+        else // si es gasto rojo y -
+        {
+            feedbackText.color = Color.red;
+            feedbackText.text = $"-{deltaAmount.ToString("F0")} Blood Echoes";
+        }
+
+        bloodechoes.gameObject.SetActive(true);
+        feedbackText.gameObject.SetActive(true);
+
+        // paro corrutina por si quedo algo
+        if (displayCoroutine != null)
+        {
+            StopCoroutine(displayCoroutine);
+        }
+
+        // inicio corrutina
+        displayCoroutine = StartCoroutine(HideFeedbackAfterDelay());
+    }
+
+    // desactivo dsp del tiempo
+    private System.Collections.IEnumerator HideFeedbackAfterDelay()
+    {
+        yield return new WaitForSeconds(DisplayDuration);
+        if (feedbackText != null)
+        {
+            bloodechoes.gameObject.SetActive(false);
+            feedbackText.gameObject.SetActive(false);
+        }
+    }
+}
