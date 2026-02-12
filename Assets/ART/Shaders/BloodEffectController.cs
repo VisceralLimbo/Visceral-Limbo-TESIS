9a71bdfe64687624cd4711375e32f0d38692cc54
Lucas-1812 <lucasetorres2013@gmail.com>
2026-02-12 16:16:28 -0300

    Mas efectos a la camara para la estetica de ps1

--- Assets/ART/Shaders/BloodEffectController.cs
+++ Assets/ART/Shaders/BloodEffectController.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
 
 public class BloodEffectController : MonoBehaviour
 {
@@ -10,6 +13,12 @@ public class BloodEffectController : MonoBehaviour
 
     private Coroutine transitionCoroutine;
     private int voronoiID;
+
+    [Header("Post Processing")]
+    [SerializeField] private Volume globalVolume;
+
+    private Bloom bloom;
+    private Coroutine bloomCoroutine;
 
     private void Awake()
     {
@@ -23,18 +32,29 @@ public class BloodEffectController : MonoBehaviour
 
         if (!bloodMaterial.HasProperty(voronoiID))
             Debug.LogWarning($"BloodEffectController: propiedad {voronoiProperty} no encontrada en {bloodMaterial.name}");
+
+        if (globalVolume != null && globalVolume.profile.TryGet(out bloom))
+        {
+            bloom.clamp.overrideState = true;
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró Bloom en el Global Volume");
+        }
     }
 
    
     public void PlayEffect()
     {
         StartTransition(-0.6f); // mínimo del slider
+        StartBloomTransition(0f); // Clamp 1 a 0
     }
 
     
     public void StopEffect()
     {
         StartTransition(0f); // máximo del slider
+        StartBloomTransition(1f); // Clamp 0 a 1
     }
 
     private void StartTransition(float targetValue)
@@ -44,6 +64,17 @@ public class BloodEffectController : MonoBehaviour
 
         transitionCoroutine = StartCoroutine(SetShaderFloatOverTime(targetValue));
     }
+
+    private void StartBloomTransition(float targetValue)
+    {
+        if (bloom == null) return;
+
+        if (bloomCoroutine != null)
+            StopCoroutine(bloomCoroutine);
+
+        bloomCoroutine = StartCoroutine(SetBloomClampOverTime(targetValue));
+    }
+
 
     private IEnumerator SetShaderFloatOverTime(float targetValue)
     {
@@ -66,6 +97,26 @@ public class BloodEffectController : MonoBehaviour
         bloodMaterial.SetFloat(voronoiID, targetValue);
 
         transitionCoroutine = null;
+    }
+
+    private IEnumerator SetBloomClampOverTime(float targetValue)
+    {
+        float startValue = bloom.clamp.value;
+        float elapsed = 0f;
+
+        while (elapsed < transitionDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / transitionDuration);
+            float newValue = Mathf.Lerp(startValue, targetValue, t);
+
+            bloom.clamp.value = newValue;
+
+            yield return null;
+        }
+
+        bloom.clamp.value = targetValue;
+        bloomCoroutine = null;
     }
 }
 
